0f318a7cb4dd8df7033e0704938ac9b0aaef692d
Umais Usmani <98653230+StarchyPepper29@users.noreply.github.com>
2023-11-10 16:41:24 +0500

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
 
-export default function App() {
+import * as React from 'react';
+import { View, Text, TextInput, Button } from 'react-native'; // Import TextInput
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen({ navigation }) {
+  const [name, setName] = React.useState('');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <TextInput
+        placeholder="Enter your name"
+        value={name}
+        onChangeText={(text) => setName(text)}
+      />
+      <Button
+        title="NEXT TO BIODATA"
+        onPress={() => navigation.navigate('BioDataName', { name })}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function BioDataScreen({ route, navigation }) {
+  const name = route.params?.name;
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>BIODATA FORM</Text>
+      <Text>Name from HomeScreen: {name}</Text>
+      <Button
+        title="NEXT TO GOALS"
+        onPress={() => navigation.navigate('GoalsDataName')}
+      />
+    </View>
+  );
+}
+
+function GoalsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>GOALS FORM</Text>
+      <Button
+        title="Back Home"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Overview' }} />
+        <Stack.Screen name="BioDataName" component={BioDataScreen} />
+        <Stack.Screen name="GoalsDataName" component={GoalsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
