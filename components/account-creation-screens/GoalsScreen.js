3f105dce70eab9c53ed615b140f6b631d6bfd27b
Umais Usmani <98653230+StarchyPepper29@users.noreply.github.com>
2023-11-14 15:40:28 +0500

    V1 Forms

--- components/account-creation-screens/GoalsScreen.js
+++ components/account-creation-screens/GoalsScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { View, Text, TextInput, Button, Picker } from 'react-native';
 
 const GoalsScreen = ({ navigation }) => {
